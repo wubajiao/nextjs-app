388e23006cad5d3883c81ba1d78bd7756926414f
hayden <1058486292@qq.com>
2023-09-28 11:17:03 +0800

    feat: UI框架由antd换成NextUI

--- src/app/(fullLayout)/home/page.tsx
+++ src/app/(fullLayout)/home/page.tsx
@@ -3,14 +3,21 @@
  * @Author       : wuhaidong
  * @Date         : 2023-09-27 12:01:56
  * @LastEditors  : wuhaidong
- * @LastEditTime : 2023-09-28 09:43:08
+ * @LastEditTime : 2023-09-28 11:15:20
  */
-import { Button } from 'antd'
+'use client'
+import React from 'react'
+import { Code } from '@nextui-org/react'
 function home() {
   return (
     <div className="home">
-      <Button>antd</Button>
       首页
+      <Code color="default">npm install @nextui-org/react</Code>
+      <Code color="primary">npm install @nextui-org/react</Code>
+      <Code color="secondary">npm install @nextui-org/react</Code>
+      <Code color="success">npm install @nextui-org/react</Code>
+      <Code color="warning">npm install @nextui-org/react</Code>
+      <Code color="danger">npm install @nextui-org/react</Code>
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
+/*
+ * @Descripttion :
+ * @Author       : wuhaidong
+ * @Date         : 2023-09-27 10:13:32
+ * @LastEditors  : wuhaidong
+ * @LastEditTime : 2023-09-28 11:13:09
+ */
 import type { Config } from 'tailwindcss'
+const { nextui } = require('@nextui-org/react')
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [],
+  darkMode: 'class',
+  plugins: [nextui()],
 }
 export default config
