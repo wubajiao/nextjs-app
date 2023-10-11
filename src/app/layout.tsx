79c78c193badaf8b1a237de9456adc291a24d48c
hayden <1058486292@qq.com>
2023-10-11 11:54:40 +0800

    feat: 添加antd，打算移除nextUI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,14 @@
  * @Author       : wuhaidong
  * @Date         : 2023-09-27 10:13:32
  * @LastEditors  : wuhaidong
- * @LastEditTime : 2023-10-08 18:06:57
+ * @LastEditTime : 2023-10-11 11:48:31
  */
-import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import StyledComponentsRegistry from '../lib/AntdRegistry'
 
 const inter = Inter({ subsets: ['latin'] })
+import './globals.css'
 
 export const metadata: Metadata = {
   title: 'leeks App',
@@ -23,7 +24,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* iconfont字体图标 */}
         <link href="//at.alicdn.com/t/c/font_4271386_ccfzxxlom8e.css" rel="stylesheet" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   )
 }
---
+++ src/lib/AntdRegistry.tsx
@@ -0,0 +1,23 @@
+/*
+ * @Descripttion :
+ * @Author       : wuhaidong
+ * @Date         : 2023-10-11 11:46:57
+ * @LastEditors  : wuhaidong
+ * @LastEditTime : 2023-10-11 11:47:14
+ */
+'use client'
+
+import React from 'react'
+import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs'
+import type Entity from '@ant-design/cssinjs/es/Cache'
+import { useServerInsertedHTML } from 'next/navigation'
+
+const StyledComponentsRegistry = ({ children }: React.PropsWithChildren) => {
+  const cache = React.useMemo<Entity>(() => createCache(), [])
+  useServerInsertedHTML(() => (
+    <style id="antd" dangerouslySetInnerHTML={{ __html: extractStyle(cache, true) }} />
+  ))
+  return <StyleProvider cache={cache}>{children}</StyleProvider>
+}
+
+export default StyledComponentsRegistry
