32b60b53bb4e9faf798a6b85bbcc821b9b66e0ad
hayden <1058486292@qq.com>
2023-11-09 16:13:59 +0800

    feat:  大屏左右两边布局优化

--- src/app/(blankLayout)/fight/ContentRight/index.tsx
+++ src/app/(blankLayout)/fight/ContentRight/index.tsx
@@ -3,7 +3,7 @@
  * @Author       : wuhaidong
  * @Date         : 2023-09-27 17:44:05
  * @LastEditors  : wuhaidong
- * @LastEditTime : 2023-11-02 21:48:55
+ * @LastEditTime : 2023-11-03 23:57:03
  */
 'use client'
 import React, { useState, useEffect } from 'react'
@@ -14,62 +14,24 @@ import Panel from '../components/Panel'
 import Icon from '../components/Icon'
 import Title from '../components/Title'
 
-const news = [
-  {
-    title: '美乌领导人白宫会晤 讨论加强军援应对冬季防务',
-    content:
-      '财联社9月22日电，美国总统拜登21日在白宫与到访的乌克兰总统泽连斯基举行会晤。泽连斯基表示，自己此行取得多项“重要成果”，包括双方就加强乌克兰军事能力达成新共识、美方将在冬季加强乌克兰的防空能力、双方就扩大乌克兰粮食出口的具体方案达成一致等。美方当天宣布，向乌克兰提供价值3.25亿美元的新一轮军事安全援助。这批援助清单中主要包括“复仇者”防空系统、防空导弹、火炮弹药以及反装甲系统等。 (中新社)',
-    time: '10:12:90',
-    id: 1,
-  },
-  {
-    title: '美乌领导人白宫会晤 讨论加强军援应对冬季防务',
-    content:
-      '财联社9月22日电，美国总统拜登21日在白宫与到访的乌克兰总统泽连斯基举行会晤。泽连斯基表示，自己此行取得多项“重要成果”，包括双方就加强乌克兰军事能力达成新共识、美方将在冬季加强乌克兰的防空能力、双方就扩大乌克兰粮食出口的具体方案达成一致等。美方当天宣布，向乌克兰提供价值3.25亿美元的新一轮军事安全援助。这批援助清单中主要包括“复仇者”防空系统、防空导弹、火炮弹药以及反装甲系统等。 (中新社)',
-    time: '10:12:90',
-    id: 2,
-  },
-  {
-    title: '美乌领导人白宫会晤 讨论加强军援应对冬季防务',
-    content:
-      '财联社9月22日电，美国总统拜登21日在白宫与到访的乌克兰总统泽连斯基举行会晤。泽连斯基表示，自己此行取得多项“重要成果”，包括双方就加强乌克兰军事能力达成新共识、美方将在冬季加强乌克兰的防空能力、双方就扩大乌克兰粮食出口的具体方案达成一致等。美方当天宣布，向乌克兰提供价值3.25亿美元的新一轮军事安全援助。这批援助清单中主要包括“复仇者”防空系统、防空导弹、火炮弹药以及反装甲系统等。 (中新社)',
-    time: '10:12:90',
-    id: 3,
-  },
-  {
-    title: '美乌领导人白宫会晤 讨论加强军援应对冬季防务',
-    content:
-      '财联社9月22日电，美国总统拜登21日在白宫与到访的乌克兰总统泽连斯基举行会晤。泽连斯基表示，自己此行取得多项“重要成果”，包括双方就加强乌克兰军事能力达成新共识、美方将在冬季加强乌克兰的防空能力、双方就扩大乌克兰粮食出口的具体方案达成一致等。美方当天宣布，向乌克兰提供价值3.25亿美元的新一轮军事安全援助。这批援助清单中主要包括“复仇者”防空系统、防空导弹、火炮弹药以及反装甲系统等。 (中新社)',
-    time: '10:12:90',
-    id: 4,
-  },
-  {
-    title: '美乌领导人白宫会晤 讨论加强军援应对冬季防务',
-    content:
-      '财联社9月22日电，美国总统拜登21日在白宫与到访的乌克兰总统泽连斯基举行会晤。泽连斯基表示，自己此行取得多项“重要成果”，包括双方就加强乌克兰军事能力达成新共识、美方将在冬季加强乌克兰的防空能力、双方就扩大乌克兰粮食出口的具体方案达成一致等。美方当天宣布，向乌克兰提供价值3.25亿美元的新一轮军事安全援助。这批援助清单中主要包括“复仇者”防空系统、防空导弹、火炮弹药以及反装甲系统等。 (中新社)',
-    time: '10:12:90',
-    id: 5,
-  },
-  {
-    title: '美乌领导人白宫会晤 讨论加强军援应对冬季防务',
-    content:
-      '财联社9月22日电，美国总统拜登21日在白宫与到访的乌克兰总统泽连斯基举行会晤。泽连斯基表示，自己此行取得多项“重要成果”，包括双方就加强乌克兰军事能力达成新共识、美方将在冬季加强乌克兰的防空能力、双方就扩大乌克兰粮食出口的具体方案达成一致等。美方当天宣布，向乌克兰提供价值3.25亿美元的新一轮军事安全援助。这批援助清单中主要包括“复仇者”防空系统、防空导弹、火炮弹药以及反装甲系统等。 (中新社)',
-    time: '10:12:90',
-    id: 6,
-  },
-]
-
 export default function ContentRight() {
   const [hotStocks, setHotstocks] = useState<any>([])
+  const [telegrams, setTelegrams] = useState<any>([])
 
   useEffect(() => {
     gethotStocks()
+    getTelegrams()
     const timer = setInterval(() => {
       gethotStocks()
     }, 60 * 1000)
 
+    const timerTelegrams = setInterval(() => {
+      getTelegrams()
+    }, 30 * 1000)
+
     return () => {
       clearInterval(timer)
+      clearInterval(timerTelegrams)
     }
   }, [])
 
@@ -80,6 +42,19 @@ export default function ContentRight() {
       .then((response) => {
         const { data } = response
         setHotstocks(data?.items || [])
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+
+  // 获取电报列表
+  const getTelegrams = () => {
+    request
+      .get(`/telegram/list?pageNum=1&pageSize=20`)
+      .then((response) => {
+        const { data } = response
+        setTelegrams(data?.list || [])
       })
       .catch((error) => {
         console.error(error)
@@ -119,13 +94,15 @@ export default function ContentRight() {
       <Panel className={styles.newsWrap}>
         <Title name="快讯"></Title>
         <div className={styles.news}>
-          {news.map((item) => {
+          {telegrams.map((item: any) => {
             return (
               <div key={item.id} className={styles.newItem}>
-                <div className={styles.left}>{item.time}</div>
-                <div className={styles.right}>
-                  <div className={styles.title}>【{item.title}】</div>
-                  <div className={styles.content}>{item.content}</div>
+                <div className={styles.time}>
+                  {item.publishTime.slice(11, 19)}
+                </div>
+                <div>
+                  <span className={styles.title}>【{item.title}】</span>
+                  <span className={styles.content}>{item.content}</span>
                 </div>
               </div>
             )
