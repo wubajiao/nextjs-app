fd72a05eb5e7f63c9cff5fbcc917a62f14d19727
hayden <1058486292@qq.com>
2023-11-09 16:13:11 +0800

    feat: 大盘资金流向布局/对接

--- src/app/(blankLayout)/fight/ContentCenter/index.tsx
+++ src/app/(blankLayout)/fight/ContentCenter/index.tsx
@@ -3,11 +3,12 @@
  * @Author       : wuhaidong
  * @Date         : 2023-09-27 17:44:05
  * @LastEditors  : wuhaidong
- * @LastEditTime : 2023-10-08 21:57:16
+ * @LastEditTime : 2023-11-09 16:08:48
  */
 'use client'
-import React, { useRef, useEffect } from 'react'
-import * as echarts from 'echarts'
+import React, { useEffect, useState } from 'react'
+import request from '@/utils/request'
+import NP from 'number-precision'
 import styles from './index.module.scss'
 import Panel from '../components/Panel'
 
@@ -37,145 +38,171 @@ const list = [
 const riseFallList = [
   {
     name: '全部',
-    current: 5310,
+    current: '--',
     type: 0,
+    key: 'all',
   },
   {
     name: '上涨',
-    current: 9,
+    current: '--',
     type: 1,
+    key: 'up',
   },
   {
     name: '涨停',
-    current: 3020,
+    current: '--',
     type: 1,
+    key: 't',
   },
   {
     name: '下跌',
-    current: 1890,
+    current: '--',
     type: 2,
+    key: 'down',
   },
   {
     name: '跌停',
-    current: 12,
+    current: '--',
     type: 2,
+    key: 'b',
   },
 ]
 
-const dealVolumeList = [
-  {
-    name: '总成交',
-    current: 5310,
-  },
-  {
-    name: '上海',
-    current: 9,
-    type: 1,
-  },
-  {
-    name: '深圳',
-    current: 3020,
-    type: 1,
-  },
-  {
-    name: '创业板',
-    current: 1890,
-    type: 2,
-  },
+const marketFlowList = [
+  { label: '今日主力净流入', value: '--' },
+  { label: '今日超大单净流入', value: '--' },
+  { label: '今日大单净流入', value: '--' },
+  { label: '今日中单净流入', value: '--' },
+  { label: '今日小单净流入', value: '--' },
 ]
 
 export default function ContentCenter() {
-  const chartRef: any = useRef()
+  const [riseFall, setRiseFall] = useState<any>([]) // 涨跌数汇总
+  const [transactionVolume, setTransactionVolume] = useState<any>([]) // 指数成交额
+  const [fundDirection, setFundDirection] = useState<any>([]) // 北上南下资金
+  const [marketFlow, setMarketFlow] = useState<any>([]) // 大盘资金流向
 
   useEffect(() => {
-    initChart()
+    getRiseFall()
+    getTransactionVolume()
+    getFundDirection()
+    getMarketFlow()
+    const timer = setInterval(() => {
+      getRiseFall()
+      getTransactionVolume()
+      getFundDirection()
+      getMarketFlow()
+    }, 60 * 1000)
+
+    return () => clearInterval(timer)
   }, [])
 
-  const initChart = () => {
-    const myChart = echarts.init(chartRef.current, 'dark')
+  // 获取涨跌幅数据
+  const getRiseFall = () => {
+    request
+      .get(`/allStock/upDownNumber`)
+      .then((response) => {
+        const { data } = response
+        const object = data[0] || {}
+        let newList = []
+        for (let i = 0; i < riseFallList.length; i++) {
+          let current = 0
+          if (riseFallList[i].key === 'all') {
+            current = object.up + object.down + object.r0
+          } else {
+            current = object[riseFallList[i].key]
+          }
+          let item = {
+            ...riseFallList[i],
+            current,
+          }
+          newList.push(item)
+        }
+        setRiseFall(newList)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
 
-    const option = {
-      xAxis: {
-        type: 'category',
-        boundaryGap: false,
-        data: [
-          '9:30',
-          '10:00',
-          '10:30',
-          '11:00',
-          '11:30',
-          '13:00',
-          '13:30',
-          '14:00',
-          '14:30',
-          '15:00',
-        ],
-      },
-      yAxis: {
-        type: 'value',
-      },
-      legend: {
-        data: ['北向资金', '沪港通', '深港通'],
-        textStyle: {
-          color: '#fff', // 图例文字颜色
-        },
-      },
-      grid: {
-        left: '3%',
-        right: '3%',
-        bottom: '3%',
-        top: '25%',
-        containLabel: true,
-      },
-      series: [
-        {
-          name: '北向资金',
-          type: 'line',
-          smooth: true,
-          showSymbol: false,
-          data: [100, 230, 224, 218, 135, 147, 260],
-          areaStyle: {
-            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-              { offset: 0, color: '#19e3b4' },
-              { offset: 1, color: '#19e3b400' },
-            ]),
-          },
-        },
-        {
-          name: '沪港通',
-          type: 'line',
-          smooth: true,
-          showSymbol: false,
-          data: [150, 250, 224, 218, 135, 147, 260],
-          areaStyle: {
-            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-              { offset: 0, color: '#0066d2' },
-              { offset: 1, color: '#0066d200' },
-            ]),
-          },
-        },
-        {
-          name: '深港通',
-          type: 'line',
-          smooth: true,
-          showSymbol: false,
-          data: [200, 230, 224, 218, 135, 147, 260],
-          areaStyle: {
-            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-              {
-                offset: 0,
-                color: 'rgb(55, 162, 255)',
-              },
-              {
-                offset: 1,
-                color: 'rgb(116, 21, 219,.5)',
-              },
-            ]),
-          },
-        },
-      ],
-    }
-    return option && myChart.setOption(option, true)
+  // 获取股指成交额
+  const getTransactionVolume = () => {
+    request
+      .get(`/allStock/batch?symbol=SH000001,SZ399001,SZ399006,SH000688`)
+      .then((response) => {
+        const {
+          data: { items },
+        } = response
+
+        let newList = []
+        let total = 0
+        for (let i = 0; i < items.length; i++) {
+          newList.push(items[i].quote)
+          total = total + items[i].quote.amount
+        }
+
+        newList.unshift({ name: '总交易额', amount: total })
+
+        setTransactionVolume(newList)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+
+  // 获取北上/南下资金流向
+  const getFundDirection = () => {
+    request
+      .get(`/allStock/hsgtDate`)
+      .then((response) => {
+        const { data } = response
+        setFundDirection(data)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+
+  // 获取大盘资金流向
+  const getMarketFlow = () => {
+    request
+      .get(`/allStock/marketFlow`)
+      .then((response) => {
+        const {
+          data: { klines },
+        } = response
+        const flowString = klines[klines.length - 1]
+        const flowArray = flowString.split(',')
+        const result = marketFlowList.map((item, index) => {
+          let value = '--'
+          switch (index) {
+            case 0:
+              value = flowArray[1]
+              break
+            case 1:
+              value = flowArray[5]
+              break
+            case 2:
+              value = flowArray[4]
+              break
+            case 3:
+              value = flowArray[3]
+              break
+            case 4:
+              value = flowArray[2]
+              break
+          }
+
+          return {
+            label: item.label,
+            value,
+          }
+        })
+        setMarketFlow(result)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
   }
 
   return (
@@ -197,7 +224,7 @@ export default function ContentCenter() {
         })}
       </Panel>
       <Panel className={styles.riseFall}>
-        {riseFallList.map((item) => {
+        {riseFall.map((item: any) => {
           return (
             <div key={item.name} className={styles.card}>
               <div className={styles.label}>{item.name}</div>
@@ -213,21 +240,45 @@ export default function ContentCenter() {
         })}
       </Panel>
       <Panel className={styles.dealVolume}>
-        {dealVolumeList.map((item) => {
+        {transactionVolume.map((item: any) => {
           return (
             <div key={item.name} className={styles.card}>
               <div className={styles.label}>{item.name}</div>
-              <div className={`${styles.number}`}>{item.current}</div>
+              <div className={`${styles.number}`}>
+                {NP.round(NP.divide(item.amount, 100000000), 2)}亿
+              </div>
             </div>
           )
         })}
       </Panel>
-      <Panel className={styles.chart}>
-        <div
-          ref={chartRef}
-          style={{ height: '100%', width: '100%' }}
-          id="chart"
-        />
+      <Panel className={styles.fundDirection}>
+        {fundDirection.map((item: any) => {
+          return (
+            <div key={item.BOARD_TYPE} className={styles.item}>
+              <div className={styles.top}>
+                <span style={{ marginRight: 6 }}>{item.BOARD_TYPE}</span>
+                <span>{item.FUNDS_DIRECTION}</span>
+              </div>
+              <div>{NP.round(NP.divide(item.dayNetAmtIn, 10000), 2)}亿</div>
+            </div>
+          )
+        })}
+      </Panel>
+      <Panel className={`${styles.dealVolume} ${styles.marketFlow}`}>
+        {marketFlow.map((item: any) => {
+          return (
+            <div key={item.name} className={styles.card}>
+              <div className={styles.label}>{item.label}</div>
+              <div
+                className={`${styles.number} ${
+                  item.value > 0 ? styles.up : styles.down
+                }`}
+              >
+                {NP.round(NP.divide(item.value, 100000000), 2)}亿
+              </div>
+            </div>
+          )
+        })}
       </Panel>
     </div>
   )
