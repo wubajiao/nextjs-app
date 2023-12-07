a6a6f0967867973320be0e60894a22f237f6ed33
hayden <1058486292@qq.com>
2023-12-07 11:54:55 +0800

    底部修改

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@
  * @Author       : wuhaidong
  * @Date         : 2023-09-27 14:59:23
  * @LastEditors  : wuhaidong
- * @LastEditTime : 2023-11-22 15:09:56
+ * @LastEditTime : 2023-12-07 11:44:01
  */
 import styles from './index.module.scss'
 
@@ -12,7 +12,8 @@ export default function Footer() {
     <div className={styles.footer}>
       <div className={styles.content}>
         <div className={styles.copyright}>
-          Copyright © 2023 深圳久财 ｜ 粤ICP备2023112209号
+          Copyright © 2023 深圳市久财科技有限公司
+          {/* ｜ 粤ICP备2023112209号 */}
         </div>
       </div>
     </div>
