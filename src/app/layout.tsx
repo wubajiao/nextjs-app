ec864053cba44997fde8b11620784624f3849526
hayden <1058486292@qq.com>
2023-12-22 16:00:40 +0800

    feat: 百度统计代码引入修改

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,15 @@
  * @Author       : wuhaidong
  * @Date         : 2023-09-27 10:13:32
  * @LastEditors  : wuhaidong
- * @LastEditTime : 2023-12-21 15:30:00
+ * @LastEditTime : 2023-12-22 15:59:32
  */
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import StyledComponentsRegistry from '../lib/AntdRegistry'
 import { ConfigProvider } from 'antd'
 import zhCN from 'antd/locale/zh_CN'
-import BaiDuAnalytics from './BaiDuAnalytics'
+// import BaiDuAnalytics from './BaiDuAnalytics.js'
+import Script from 'next/script'
 
 import './globals.css'
 
@@ -48,10 +49,10 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
         />
+        <Script src="./BaiDuAnalytics.js" />
       </head>
       <body className={inter.className}>
         <StyledComponentsRegistry>
-          <BaiDuAnalytics />
           <ConfigProvider
             locale={zhCN}
             theme={{
